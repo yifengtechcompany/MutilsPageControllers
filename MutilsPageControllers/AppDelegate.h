97b3d72f3b3764a07c1fedcba213c51ec04e4195
Kuntanury <kuntanury@gmail.com>
2015-08-26 18:56:47 +0800

    Create
    Hello YiFeng~

---
+++ MutilsPageControllers/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  MutilsPageControllers
+//
+//  Created by Kuntanury on 15/8/26.
+//  Copyright © 2015年 YiFengTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ MutilsPageControllersTests/MutilsPageControllersTests.m
@@ -0,0 +1,39 @@
+//
+//  MutilsPageControllersTests.m
+//  MutilsPageControllersTests
+//
+//  Created by Kuntanury on 15/8/26.
+//  Copyright © 2015年 YiFengTech. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MutilsPageControllersTests : XCTestCase
+
+@end
+
+@implementation MutilsPageControllersTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
